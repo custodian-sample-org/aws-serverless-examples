eb1b33fbbb0a066727417c4e35e4f291fc365734
Morgante Pell <me@morgante.net>
2023-08-05 23:24:03 -0400

    apply the pattern

--- aws-node/index.js
+++ aws-node/index.js
@@ -1,13 +1,15 @@
-module.exports.handler = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
+const encoder = new TextEncoder('utf-8');
+
+export async function handleRequest(request) {
+    return {
+    status: 200,
+    body: encoder.encode(JSON.stringify(
       {
         message: 'Go Serverless v3.0! Your function executed successfully!',
-        input: event,
+        input: request,
       },
       null,
       2
-    ),
+    )).buffer,
   };
-};
+}
